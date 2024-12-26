67567e3a6796abbf62a6bcb47ec42b2fdcbe92ba
Dhruv-1511 <dhruvsheladiya07@gmail.com>
2024-12-26 13:30:26 +0530

    routing change

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoIosArrowUp, IoMdClose } from "react-icons/io";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -26,29 +27,29 @@ const Navbar = () => {
 
   return (
     <div className="flex flex-wrap justify-between items-center mt-10 px-28 lg:px-8">
-      <a href="/">
+      <Link to="/">
         <div className="flex items-center gap-2">
           <img src="logo.png" alt="logo" className="w-9 h-9 lg:w-7 lg:h-7" />
           <p className="text-5xl font-bold lg:text-3xl">Positivus</p>
         </div>
-      </a>
+      </Link>
 
       <nav className="flex flex-wrap items-center mt-4 gap-8 text-xl xl:gap-4 md:hidden">
-        <a href="/about" onClick={() => setMenu("/about")} className={`hover:underline rounded-lg ${isActive("/about")}`}>
+        <Link to="/about" onClick={() => setMenu("/about")} className={`hover:underline rounded-lg ${isActive("/about")}`}>
           About us
-        </a>
-        <a href="/service" onClick={() => setMenu("/service")} className={`hover:underline rounded-lg ${isActive("/service")}`}>
+        </Link>
+        <Link to="/service" onClick={() => setMenu("/service")} className={`hover:underline rounded-lg ${isActive("/service")}`}>
           Services
-        </a>
-        <a href="/usecase" onClick={() => setMenu("/usecase")} className={`hover:underline rounded-lg ${isActive("/usecase")}`}>
+        </Link>
+        <Link to="/usecase" onClick={() => setMenu("/usecase")} className={`hover:underline rounded-lg ${isActive("/usecase")}`}>
           Use Cases
-        </a>
-        <a href="/price" onClick={() => setMenu("/price")} className={`hover:underline rounded-lg ${isActive("/price")}`}>
+        </Link>
+        <Link to="/price" onClick={() => setMenu("/price")} className={`hover:underline rounded-lg ${isActive("/price")}`}>
           Pricing
-        </a>
-        <a href="/blog" onClick={() => setMenu("/blog")} className={`hover:underline rounded-lg ${isActive("/blog")}`}>
+        </Link>
+        <Link to="/blog" onClick={() => setMenu("/blog")} className={`hover:underline rounded-lg ${isActive("/blog")}`}>
           Blog
-        </a>
+        </Link>
 
         <button className="border-2 border-black px-6 py-3 rounded-lg hover:bg-black hover:text-white lg:px-5 lg:py-2">
           Request a quote
@@ -65,41 +66,36 @@ const Navbar = () => {
             <p className="text-2xl font-bold text-black">Menu</p>
             <IoMdClose className="cursor-pointer text-black" onClick={toggleSidebar} />
           </div>
-          <a
-            href="/about"
+          <Link to="/about"
             onClick={() => setMenu("/about")}
             className={`text-black hover:underline text-xl font-semibold ${isActive("/about")}`}
           >
             About us
-          </a>
-          <a
-            href="/service"
+          </Link>
+          <Link to="/service"
             onClick={() => setMenu("/service")}
             className={`text-black hover:underline text-xl font-semibold ${isActive("/service")}`}
           >
             Services
-          </a>
-          <a
-            href="/usecase"
+          </Link>
+          <Link to="/usecase"
             onClick={() => setMenu("/usecase")}
             className={`text-black hover:underline text-xl font-semibold ${isActive("/usecase")}`}
           >
             Use Cases
-          </a>
-          <a
-            href="/price"
+          </Link>
+          <Link to="/price"
             onClick={() => setMenu("/price")}
             className={`text-black hover:underline text-xl font-semibold ${isActive("/price")}`}
           >
             Pricing
-          </a>
-          <a
-            href="/blog"
+          </Link>
+          <Link to="/blog"
             onClick={() => setMenu("/blog")}
             className={`text-black hover:underline text-xl font-semibold ${isActive("/blog")}`}
           >
             Blog
-          </a>
+          </Link>
           <button className="text-black border-2 border-black px-6 py-3 rounded-lg hover:bg-black hover:text-white">
             Request a quote
           </button>
